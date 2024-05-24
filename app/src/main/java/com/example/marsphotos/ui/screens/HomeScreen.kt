a63ff74565067e351e91f14ef31f178c13b67ecb
solderq35 <solderq35@gmail.com>
2024-05-24 01:29:04 -0700

    switch to text in cards

--- app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -35,14 +35,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.example.marsphotos.R
 import com.example.marsphotos.model.MarsPhoto
 import com.example.marsphotos.ui.theme.MarsPhotosTheme
@@ -118,17 +114,11 @@ fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        AsyncImage(
-            model =
-                ImageRequest.Builder(context = LocalContext.current)
-                    .data(photo.imgSrc) // change from image card to text string HERE
-                    .crossfade(true)
-                    .build(),
-            error = painterResource(R.drawable.ic_broken_image),
-            placeholder = painterResource(R.drawable.loading_img),
-            contentDescription = stringResource(R.string.mars_photo),
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth()
+        Text(
+            text = photo.imgSrc,
+            modifier =
+                Modifier.padding(16.dp) // Add padding inside the card
+                    .fillMaxWidth() // Make the text fill the width of the card
         )
     }
 }
